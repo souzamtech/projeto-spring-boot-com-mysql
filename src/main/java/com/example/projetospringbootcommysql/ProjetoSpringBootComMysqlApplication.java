7f8ed71095af003bab7dafbeee54d43264b7b0cf
Matheus <matheussouza102030@gmail.com>
2026-08-04 21:05:48 -0300

    Initial commit

---
+++ src/main/java/com/example/projetospringbootcommysql/ProjetoSpringBootComMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetospringbootcommysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootComMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootComMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetospringbootcommysql/ProjetoSpringBootComMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetospringbootcommysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootComMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
